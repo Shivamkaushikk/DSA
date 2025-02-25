a30db870a1b0c26b1e3765af31a91a8a2108abcd
Shivamkaushikk <shivamkaushikk444@gmail.com>
2025-02-25 17:11:32 +0530

    Patternsstart

---
+++ Patterns.cpp
@@ -0,0 +1,156 @@
+// 21 patterns 
+
+// RULES FOR MAKING PATTERNS ---> NESTED LOOPS ---> MAINLY 2 LOOPS
+
+// ---> 1) for the outer loop count the number of rows 
+// ---> 2) for the inner loop focus on the colums & connect them somehow to the rows
+// ---> 3) Print then "*" inside the inner for loop
+// ---> 4) observe symmetry { OPTIONAL }
+
+/* Pattern 1 --->
+****
+****
+****
+****
+*/
+
+#include <iostream>
+using namespace std;
+
+void print1(int n){
+    for(int i=0; i<n;i++){
+        for(int j=0;j<n;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
+
+int main(){
+    int n;
+    cin>>n;
+    print1(n);
+}
+
+/*---------------------------------------------------------------------------------------*/
+
+/* Pattern 2
+*
+**
+***
+****
+*/
+
+
+#include <iostream>
+using namespace std;
+
+void print1(int n){
+    for(int i=0; i<n;i++){
+        for(int j=0;j<=i;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
+
+int main(){
+    int n;
+    cin>>n;
+    print1(n);
+}
+
+/*--------------------------------------------------------------------------------------*/
+
+/* Pattern 3
+1
+12
+123
+1234
+*/
+
+
+#include <iostream>
+using namespace std;
+
+void print1(int n){
+    for(int i=0; i<=n;i++){
+        for(int j=1;j<=i;j++){
+            cout<<j;
+        }
+        cout<<endl;
+    }
+}
+
+int main(){
+    int n;
+    cin>>n;
+    print1(n);
+}
+
+/*----------------------------------------------------------------------------------*/
+
+/*Pattern 5
+****
+***
+**
+*
+*/
+
+
+#include <iostream>
+using namespace std;
+
+void print1(int n){
+    for(int i=0; i<n;i++){  //n=4
+        for(int j=n;j>i;j--){ 
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
+
+int main(){
+    int n;
+    cin>>n;
+    print1(n);
+}
+
+/*----------------------------------------------------------------------------------*/
+
+/*Pattern 6
+1234
+123
+12
+1
+*/
+
+#include <iostream>
+using namespace std;
+
+void print1(int n){
+    for(int i=1; i<=n;){  //n=4
+        for(int j=i;j<=n;j++){ 
+            cout<<j;
+        }
+        n--;
+        cout<<endl;
+    }
+}
+
+int main(){
+    int n;
+    cin>>n;
+    print1(n);
+}
+
+/*-------------------------------------------------------------------------------*/
+
+/* Pattern 7
+    *
+   ***
+  *****
+ *******   
+*/
+
+
